9448d10f488e717b87afa4943ad7bce35e886a86
seenusneha4 <seenaannmathew6@gmail.com>
2022-05-18 13:38:12 +0530

    navupdated

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addpost from './Components/Addpost';
 import Searchpost from './Components/Searchpost';
+import Header from './Components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
      <div>
+       <BrowserRouter>
+       <Routes>
+       <Route path="/" exact element={<Addpost/>}/>
+       <Route path="/search" exact element={<Searchpost/>}/>
+       
+       
 
-    <Addpost/>
-    <Searchpost/>
+       </Routes>
+       
+       </BrowserRouter>
+
+    
     </div>
   );
 }
